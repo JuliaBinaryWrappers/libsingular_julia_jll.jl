3c4a335e7021bda5f325147f2f1e6dbd94bc764b
jlbuild <juliabuildbot@gmail.com>
2026-01-09 12:40:35 +0000

    libsingular_julia_jll build 0.47.10+0

--- src/libsingular_julia_jll.jl
+++ src/libsingular_julia_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libsingular_julia")
-JLLWrappers.@generate_main_file("libsingular_julia", UUID("ae4fbd8f-ecdb-54f8-bbce-35570499b30e"))
+JLLWrappers.@generate_main_file("libsingular_julia", Base.UUID("ae4fbd8f-ecdb-54f8-bbce-35570499b30e"))
 end  # module libsingular_julia_jll
